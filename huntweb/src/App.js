b267dd0cdf20d8b44db71e1b80fbbc0c004fe439
Léo Serrão Barbosa <leo_serrao@hotmail.com>
2020-08-11 18:34:08 -0300

    Buscando dados de uma API

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 
+import api from "./services/api";
+
 import './styles.css';
 
 import Header from './components/Header';
 
+import Main from './pages/main';
+
 const App = () => (
   <div className="App">
       <Header />
+      <Main />
   </div>
 );
 
